f1c5539839cf3c6020488ee370e5b012be29c4d4
faraja17 <farajakthompson@gmail.com>
2022-09-05 22:39:16 -0400

    added comments

--- game.js
+++ game.js
@@ -8,9 +8,9 @@ const nextSequence = () => {
     
     gamePattern.push(randomChosenColor);
 
-    $(document).on("keypress",function() {
+    $(document).on("keypress",function() { //keypress starts game (user must interact for audio to work)
 
-        $(`#${randomChosenColor}`).fadeOut(100).fadeIn(100);
+        $(`#${randomChosenColor}`).fadeOut(100).fadeIn(100); //flashes random button
 
         let audio = new Audio(`sounds/${randomChosenColor}.mp3`);
         audio.play();
