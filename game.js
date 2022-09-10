389fbb41bf92c4be4f7e418a76523995e6e26d43
faraja17 <farajakthompson@gmail.com>
2022-09-10 16:48:34 -0400

    Step 8 complete

--- game.js
+++ game.js
@@ -1,26 +1,38 @@
 const buttonColors = ["red", "blue", "green", "yellow"];
 const gamePattern = [];  
-const userClickedPattern = [];
+let userClickedPattern = [];
 let level = 0;
 
 const nextSequence = () => {
+    
     randomNumber = Math.floor(Math.random() * 4);
 
     let randomChosenColor = buttonColors.at(randomNumber);
-    
+   
     gamePattern.push(randomChosenColor);
-        $(`#${randomChosenColor}`).fadeOut(100).fadeIn(100); //flashes random button
-        playSound(randomChosenColor);
-        level++;
-        $(".level-title").text(`Level ${level}`);
-        console.log(level);
+        
+    $(`#${randomChosenColor}`).fadeOut(100).fadeIn(100); //flashes random button
+    playSound(randomChosenColor);
+    level++;
+    console.log(level);
+
+    $("#level-title").text(`Level ${level}`); //changes the h1
+    
+    userClickedPattern = [];
 }
+
+//user game play
 
 $(".btn").on("click", function() {
     let userChosenColor = this.id;
     userClickedPattern.push(userChosenColor);
+    let lastAnswer = userClickedPattern[userClickedPattern.length - 1];
+    let gamePrompt = gamePattern[gamePattern.length - 1];
     playSound(userChosenColor);
     animatePress(userChosenColor);
+    if (userClickedPattern.length === gamePattern.length) { //allows user to keep playing until player array length matches game array
+        checkAnswer(lastAnswer, gamePrompt);
+    }
 });
 
 const playSound = (input) => {
@@ -34,6 +46,20 @@ const animatePress = (currentColor) => {
         setTimeout(function() {
             $(`.${currentColor}`).removeClass("pressed");
         }, 100);
+}
+
+const checkAnswer = (currentLevel, gamePrompt) => {
+    if (currentLevel === gamePrompt) {
+        console.log("success");
+
+        setTimeout(function() {
+            nextSequence();
+        }, 1000); 
+    } else {
+        console.log("wrong");
+    }
+    console.log(userClickedPattern, gamePattern)
+    
 }
 
 $(document).one("keypress",function() { //keypress starts game (user must interact for audio to work)        
